19d3585917f1dc816d5a2858f3a16b65c812e705
harvey <harvey.chen@activenetwork.com>
2017-03-03 00:15:04 +0800

    no message

--- Framework/LeaRun.Application/LeaRun.Application.Web/App_Start/RouteConfig.cs
+++ Framework/LeaRun.Application/LeaRun.Application.Web/App_Start/RouteConfig.cs
@@ -91,7 +91,7 @@ namespace LeaRun.Application.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Books", id = UrlParameter.Optional }
             );
         }
     }
